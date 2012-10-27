08bbc64b9da5cad1ba1884654b9adae83c49f9c7
James Salt <jsalt@jsalt-Inspiron-1011.(none)>
2012-10-27 20:24:23 +0100

    Speedup by using list over string

--- apitester/generators.py
+++ apitester/generators.py
@@ -5,16 +5,16 @@ from random import randint
 class Generator(object):
     def __init__(self, predicates, shortest, longest, all_possible=None):
         self.VALID, self.INVALID = self._get(predicates, options=all_possible)
-        self.SHORTEST = -1 if shortest is None else shortest
-        self.LONGEST = -1 if longest is None else longest
-        
+        self.SHORTEST = shortest
+        self.LONGEST = longest
+
     def valid(self):
         length = randint(self.SHORTEST, self.LONGEST)
-        r = ""
+        r = []
         while len(r) < length:
-            r = r + self.VALID[randint(0, len(self.VALID) - 1)]
+            r.append(self.VALID[randint(0, len(self.VALID) - 1)])
 
-        return r
+        return "".join(r)
 
     def invalid(self):
         if self.SHORTEST is 0:
@@ -23,11 +23,11 @@ class Generator(object):
             l_index = randint(0,1)
         length = [randint(self.LONGEST + 1, self.LONGEST + 10),
                 randint(0, min(self.SHORTEST - 1, 9))][l_index]
-        r = ""
+        r = []
         while len(r) < length:
-            r = r + self.INVALID[randint(0, len(self.INVALID) - 1)]
+            r.append(self.INVALID[randint(0, len(self.INVALID) - 1)])
 
-        return r
+        return "".join(r)
 
     def _get(self, predicates, options=None):
         v = []
